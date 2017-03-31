319d02f8e6ac2d0c7c360faa93bbd780b626edf0
Alan Zhang <Alan Zhang>
2017-03-31 15:59:24 -0400

    New Report capability

--- reports/urls.py
+++ reports/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.detail, name='index'),
+    url(r'^new$', views.newreport, name='new'),
     url(r'^(?P<report_id>[0-9]+)/$', views.detail, name='detail'),
 ]
