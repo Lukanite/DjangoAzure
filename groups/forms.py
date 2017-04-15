68594accb5106c27dd7d1f6ade0175fdcbd663e3
jas5mg <jas5mg@virginia.edu>
2017-04-15 15:09:58 -0400

    Fixed typo and working on creating group form

---
+++ groups/forms.py
@@ -0,0 +1,11 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from django.contrib.auth.models import Group
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
