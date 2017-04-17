8258bd4a48b6481fc3fec18b46fd1bb450cf02e4
Kaytay <kmv3qt@virginia.edu>
2017-04-17 18:21:05 -0400

    Groups can now have spaces

--- groups/urls.py
+++ groups/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.groups, name='index'),
     #url(r'^create_group$', views.create_group, name='create_group'),
-    url(r'^(?P<name>[a-zA-Z0-9]+)/$', views.group_users, name='gr_users'),
+    url(r'^(?P<name>[a-zA-Z0-9\s]+)/$', views.group_users, name='gr_users'),
 ]
