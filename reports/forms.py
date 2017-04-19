858984a64be91bb42b7e5511c76ca98f9ae861b1
Alan Zhang <alan@lucar.io>
2017-04-18 21:05:01 -0400

    Multiple file uploads!

--- reports/forms.py
+++ reports/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
-from reports.models import Report
+from reports.models import Report, ReportAttachment
 
 class ReportForm(ModelForm):
     class Meta:
-            model = Report
-            exclude = ['release_date', 'attachmenthash']
-            
+        model = Report
+        exclude = ['release_date']
+
+class ReportAttachmentForm(ModelForm):
+    class Meta:
+        model = ReportAttachment
+        labels = {
+            'attachment': ('Attachment(s)'),
+        }
+        exclude = ['report', 'attachmenthash']
