13df85e6587485b41803a839c2d833e01a687e5e
Alan Zhang <az6wf@virginia.edu>
2017-05-01 17:53:03 -0400

    Delete null option for groups

--- reports/forms.py
+++ reports/forms.py
@@ -7,13 +7,17 @@ class ReportForm(ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super(ReportForm, self).__init__(*args, **kwargs)
+        self.fields['industry'].required = False
+        self.fields['sector'].required = False
         if (user.profile.user_type == 'site_manager'):
             self.fields['group'] = ModelChoiceField(
-                queryset=Group.objects.all()
+                queryset=Group.objects.all(),
+                empty_label=None
             )
         else:
             self.fields['group'] = ModelChoiceField(
-                queryset=user.groups.filter()
+                queryset=user.groups.filter(),
+                empty_label=None
             )
 
     class Meta:
